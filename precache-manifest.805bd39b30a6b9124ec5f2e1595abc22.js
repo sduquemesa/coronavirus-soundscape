8c4b81e023585f58f1eda969a6d15623633250d9
sduquemesa <675763+sduquemesa@users.noreply.github.com>
2020-06-10 17:30:25 -0500

    Updates

---
+++ precache-manifest.805bd39b30a6b9124ec5f2e1595abc22.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ba7e1a213ac5d67a60f",
+    "url": "/coronavirus-soundscape/static/css/main.756b107e.chunk.css"
+  },
+  {
+    "revision": "6ba7e1a213ac5d67a60f",
+    "url": "/coronavirus-soundscape/static/js/main.6ba7e1a2.chunk.js"
+  },
+  {
+    "revision": "a0f14858a21a76bafde5",
+    "url": "/coronavirus-soundscape/static/js/1.a0f14858.chunk.js"
+  },
+  {
+    "revision": "3907f023d1c8d6c9e19d",
+    "url": "/coronavirus-soundscape/static/js/runtime~main.3907f023.js"
+  },
+  {
+    "revision": "87c10fa927e2b8089ec972c120aac80d",
+    "url": "/coronavirus-soundscape/index.html"
+  }
+];
